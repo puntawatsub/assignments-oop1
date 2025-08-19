85a4c9379dda549a6cb4f814604a20c97306bc13
Puntawat Subhamani <puntawatsub@gmail.com>
2025-08-19 04:16:57 +0300

    modified code + more tasks

--- src/assignment1/three/SumOfThreeNumbers.java
+++ src/assignment1/three/SumOfThreeNumbers.java
@@ -16,13 +16,14 @@ public class SumOfThreeNumbers {
         System.out.print("Give the third number: ");
         int third = Integer.parseInt(scanner.nextLine());
 
-        // Sum
+        // Sum (addition)
         int sum = first + second + third;
         // Product = multiplication
         int product = first * second * third;
-        // average = sum/number of values
+        // average = sum/number of values, in this case there are three. Divided by a float to result in float (double)
         double average = sum / 3.0;
 
+        // print out the results
         System.out.println("The sum of the numbers is " + sum);
         System.out.println("The product of the numbers is " + product);
         System.out.println("The average of the numbers is " + average);
--- src/assignment1/two/Task2.java
+++ src/assignment1/two/Task2.java
@@ -2,9 +2,6 @@ package assignment1.two;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.print("    *\n" +
-                "   ***\n" +
-                "  *****\n" +
-                " *******");
+        System.out.print("    *\n   ***\n  *****\n *******");
     }
 }
