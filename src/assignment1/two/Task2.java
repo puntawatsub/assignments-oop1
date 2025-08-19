a6c0fe75a21fbd7807168e12871489dfb5625c07
Puntawat Subhamani <puntawatsub@gmail.com>
2025-08-19 03:55:03 +0300

    re-commit
    yeah, I messed things up

---
+++ src/assignment1/two/Task2.java
@@ -0,0 +1,10 @@
+package assignment1.two;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.print("    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******");
+    }
+}
