e45f957dcb5d65b3ee0fd0d619301b24a0616bc9
Puntawat Subhamani <puntawatsub@gmail.com>
2025-08-21 15:26:16 +0300

    Update MultiplicationTableExam.java

--- src/assignment1_3/four/MultiplicationTableExam.java
+++ src/assignment1_3/four/MultiplicationTableExam.java
@@ -8,10 +8,9 @@ public class MultiplicationTableExam {
         return (int) (Math.random() * 10) + 1;
     }
 
-    static boolean exam(int num1, int num2) {
+    static boolean exam(int num1, int num2, Scanner sc) {
         int correctAnswer = num1 * num2;
 
-        Scanner sc = new Scanner(System.in);
         System.out.printf("%d x %d = ", num1, num2);
         int answer = Integer.parseInt(sc.nextLine());
 
@@ -19,23 +18,29 @@ public class MultiplicationTableExam {
     }
 
     public static void main(String[] args) {
-        System.out.println("Multiplication Exam");
-        int point = 0;
-        for (int i = 0; i < 10; i++) {
-            boolean isCorrect = exam(generateRandomNumber(), generateRandomNumber());
-            if (isCorrect) {
-                point++;
-                System.out.println("Correct");
-            } else {
-                System.out.println("Incorrect");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Multiplication Exam");
+            int point = 0;
+            for (int i = 0; i < 10; i++) {
+                boolean isCorrect = exam(generateRandomNumber(), generateRandomNumber(), sc);
+                if (isCorrect) {
+                    point++;
+                    System.out.println("Correct");
+                } else {
+                    System.out.println("Incorrect");
+                }
             }
-        }
-        if (point == 10) {
-            System.out.println("Score: 10, Congratulations on mastering the multiplication table!");
-            return;
+            if (point == 10) {
+                System.out.println("Score: 10, Congratulations on mastering the multiplication table!");
+                break;
+            }
+
+            System.out.printf("Score: %d, Try again!\n", point);
         }
 
-        System.out.printf("Score: %d, Try again!\n", point);
-        main(args);
+        sc.close();
+
     }
 }
