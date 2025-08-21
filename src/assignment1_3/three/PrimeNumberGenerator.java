430e16c0552e1f1f1bee53b1e1925c3939488ae5
PuntawatSub <puntawatsub@gmail.com>
2025-08-21 08:58:18 +0300

    update PrimeNumberGenerator.java

--- src/assignment1_3/three/PrimeNumberGenerator.java
+++ src/assignment1_3/three/PrimeNumberGenerator.java
@@ -1,7 +1,9 @@
 package assignment1_3.three;
 
+import java.util.Scanner;
+
 public class PrimeNumberGenerator {
-    public boolean isPrime(int n) {
+    public static boolean isPrime(int n) {
         if (n <= 1) {
             return false;
         } else if (n == 2) {
@@ -9,7 +11,7 @@ public class PrimeNumberGenerator {
         } else if (n % 2 == 0) {
             return false;
         } else {
-            for  (int i = 3; i * i <= Math.sqrt(n); i += 2) {
+            for  (int i = 3; i <= Math.sqrt(n); i += 2) {
                 if (n % i == 0) {
                     return false;
                 }
@@ -18,6 +20,22 @@ public class PrimeNumberGenerator {
         }
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start, end;
+        do {
+            System.out.print("Enter a positive starting number: ");
+            start = Integer.parseInt(sc.nextLine());
+        } while (start < 1);
+        do {
+            System.out.print("Enter a positive ending number: ");
+            end = Integer.parseInt(sc.nextLine());
+        } while (end <= start);
 
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
     }
 }
